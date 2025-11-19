2b16cdf5d73f52e9887cbd2875ee46021c66db4e
amanku0010-lab <aman.ku0010@gmail.com>
2025-11-19 12:05:39 +0530

    Create Program7.java

---
+++ Program7.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class MatrixAddSub {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] A = new int[rows][cols];
+        int[][] B = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+        int[][] diff = new int[rows][cols];
+
+        
+        System.out.println("Enter elements of Matrix A:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        System.out.println("Enter elements of Matrix B:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+       
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = A[i][j] + B[i][j];
+                diff[i][j] = A[i][j] - B[i][j];
+            }
+        }
+
+        
+        System.out.println("\nSum of Matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        
+        System.out.println("\nSubtraction of Matrices (A - B):");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(diff[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
