f3983fc07b61047f626b731e9f9468003b032b1a
amanku0010-lab <aman.ku0010@gmail.com>
2025-11-21 11:20:49 +0530

    mainpackage.java

---
+++ INTERMEDIATE LEVEL /Import pakage/mainpackage.java
@@ -0,0 +1,11 @@
+package mainpackage;   
+import mypackage.Helper;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Helper helper = new Helper();
+        helper.displayMessage();
+        
+        System.out.println("This is the MainProgram in mainpackage.");
+    }
+}
